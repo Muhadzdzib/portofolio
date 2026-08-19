cab66f833d35955e305d4db105873ced1a46ff97
Galang Hardy Muhadzdzib <63169417+Muhadzdzib@users.noreply.github.com>
2026-08-19 22:26:46 +0800

    Modify Vite config for relative base path
    Updated base path to relative for root domain and removed Tailwind CSS plugin.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/portofolio/",
+  base: './', // Menggunakan path relatif agar aman di domain root
+  plugins: [react()],
 });
